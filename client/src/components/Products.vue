34e445b08772346e30c26a6274107bf26f63d678
Cysteine <gbengapeace2001@gmail.com>
2022-02-06 22:05:44 +0100

    Update Product view

--- client/src/components/Products.vue
+++ client/src/components/Products.vue
@@ -2,13 +2,13 @@
     <div v-if="product != ''">
         <ul>
             <li>
-                <img :src="product[0].photo" :alt="product[0].title">
+                <img :src="product.photo" :alt="product.title">
             </li>
-            <li>Title: {{ product[0].title }}</li>
-            <li>Description: {{ product[0].description }}</li>
-            <li>Price: {{ product[0].price }}</li>
-            <li>Stock Quantity: {{ product[0].stockQuantity }}</li>
-            <li>Time: {{ product[0].createdAt }}</li>
+            <li>Title: {{ product.title }}</li>
+            <li>Description: {{ product.description }}</li>
+            <li>Price: {{ product.price }}</li>
+            <li>Stock Quantity: {{ product.stockQuantity }}</li>
+            <li>Time: {{ product.createdAt }}</li>
         </ul>
     </div>
 </template>
