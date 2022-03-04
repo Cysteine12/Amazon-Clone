3d5132d086be603943b7912561e0c780ac69ad98
Cysteine <gbengapeace2001@gmail.com>
2022-03-04 21:56:18 +0100

    commit

--- server/index.js
+++ server/index.js
@@ -19,7 +19,11 @@ connectDB()
 //=======Middleware======//
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors())
+app.use(
+    cors({
+        origin: '*'
+    })
+)
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
 app.use(session({
